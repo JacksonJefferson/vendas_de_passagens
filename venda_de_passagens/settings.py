fa26c48805885a899df327f93a988b1bdd214535
Your Name <you@example.com>
2019-07-05 20:15:12 -0300

    Criacao de urls, views

--- venda_de_passagens/settings.py
+++ venda_de_passagens/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
+    'viagens.apps.ViagensConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
